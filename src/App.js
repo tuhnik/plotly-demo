376f1d5fc9b1044055b30db67caa2ca38e5bec4f
tuhnik <huntlarsen@gmail.com>
2018-10-13 20:06:13 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,27 +1,91 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Plot from 'react-plotly.js';
+import axios from 'axios';
+import {SimpleSelect} from 'react-selectize';
 import './App.css';
 
 class App extends Component {
+  state={
+    data: {},
+    country: "Estonia",
+    countries: [],
+    year: 2018,
+    title: "",
+    x: [],
+    y: []
+  }
+
+  componentDidMount(){
+    this.getData()
+    this.getCountries()
+  }
+  getCountries(){
+    axios.get(`http://api.population.io/1.0/countries/`)
+      .then(res => {
+        
+      this.setState({countries: res.data.countries})
+      })
+    
+    }
+
+    
+  
+  getData(){
+    fetch(`http://api.population.io/1.0/population/${this.state.year}/${this.state.country}/?format=json`)
+      .then(response => response.json())
+      .then((data) => {
+        if(data.detail){
+          console.log(data.detail)
+          return;
+        }
+        this.setState({ data }, ()=>{
+        this.dataToPlot()
+      })}
+    
+    )
+  }
+  dataToPlot(){
+    let x = []
+    let y = []
+    this.state.data.forEach(el=>{
+      x.push(el.age)
+      y.push(el.total)
+    })
+    this.setState({x, y})
+    this.setState({title: this.state.country +", " +this.state.year})
+
+  }
+  updateYear(evt){
+    this.setState({year: evt.value}, ()=> this.getData())
+  }
+
+  updateCountry(evt){
+    if(!evt){
+      return;
+    }
+    this.setState({country: evt.value}, ()=> this.getData());
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    let countryOptionItems = this.state.countries.map((country, i) =>
+                <option key={i} value={country}>{country}</option>
+            );
+    let yearOptionItems = [...Array(151)].map((el, i) => 
+      <option key={i} value={"" + (i+1950) + ""}>{""+ (i+1950) + ""}</option>
+    );      
+    return <div className="App">
+
+        <div className = "header"><h3>api.population.io</h3>Retrieve population tables for a given year and country. Returns tables for all ages from 0 to 100.</div>
+         <Plot data={[{ y: this.state.y, type: "line" }]} layout={{ width: "100%", height: "100%", title: this.state.title }} />
+        <div className="controls">
+        <SimpleSelect placeholder="Change country" onValueChange={value => this.updateCountry(value)}>
+         {countryOptionItems}
+        </SimpleSelect>
+        <SimpleSelect placeholder="Change year" onValueChange={value => this.updateYear(value)}>
+          {yearOptionItems}
+        </SimpleSelect>
+        </div>
       </div>
-    );
+  
   }
 }
 
